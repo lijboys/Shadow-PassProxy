422bd80169316b900873efafd093a8ad875ce637
lijboys <97435680+lijboys@users.noreply.github.com>
2026-02-25 23:03:19 +0800

    Create _worker.js

---
+++ _worker.js
@@ -0,0 +1,9 @@
+#反代pages域名
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    // 直接读取你在网页版设置的环境变量 url
+    url.hostname = env.url; 
+    return fetch(new Request(url, request));
+  }
+}
